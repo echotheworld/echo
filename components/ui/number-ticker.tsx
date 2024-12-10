5701324c82fdeaa18233f10b3d246f98ee22137f
echo <jrchxai@gmail.com>
2024-12-10 22:50:59 +0800

    v2 - hehe

--- components/ui/number-ticker.tsx
+++ components/ui/number-ticker.tsx
@@ -1,16 +1,23 @@
 "use client"
 
 import { useEffect, useRef } from "react"
-import { motion, useSpring, useTransform } from "framer-motion"
+import { motion, useSpring, useTransform, useInView } from "framer-motion"
 
 export default function NumberTicker({ value }: { value: number }) {
   const ref = useRef<HTMLParagraphElement>(null)
-  const motionValue = useSpring(0, { duration: 1000 })
+  const isInView = useInView(ref, { once: true, amount: 0.5 })
+  
+  const motionValue = useSpring(0, { 
+    duration: 2000,
+    bounce: 0.2
+  })
   const rounded = useTransform(motionValue, (latest) => Math.round(latest))
 
   useEffect(() => {
-    motionValue.set(value)
-  }, [motionValue, value])
+    if (isInView) {
+      motionValue.set(value)
+    }
+  }, [motionValue, value, isInView])
 
   return (
     <motion.span ref={ref}>
